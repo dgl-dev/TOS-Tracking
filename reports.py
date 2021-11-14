d4222a08e1bc36df852854e5f902c9c89283c644
dgl-dev <les@dunawaygroupllc.com>
2021-11-14 17:57:05 -0500

    Added modules loadTOS.py, loadRecc.py and reports.py

---
+++ reports.py
@@ -0,0 +1,17 @@
+"""
+Create and display trading reports.
+
+Reports are provided for the specified date range. Giving the trades and their results.
+
+Args:
+    dates (Date[]): From & to dates for report (inclusive)
+
+Returns:
+    Flask screen(s)
+
+Raises:
+    Exception: FileNotFoundError() if databases are not found - see README
+
+"""
+def main(dates):
+    pass
